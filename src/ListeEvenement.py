9f95bddece2889800384a9c91dfc45d89b9f34b5
leonnie1307 <100034158+leonnie1307@users.noreply.github.com>
2022-03-18 11:59:33 +0100

    ajoue du temps

--- src/ListeEvenement.py
+++ src/ListeEvenement.py
@@ -13,64 +13,64 @@ class ListeEvenement:
      "event1":{
             "titre":"patrimoine",
             "oui":{
-                    "pop":700,"legalite":50
+                    "pop":700,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":100,"legalite":50
+                    "pop":100,"legalite":50, "temps":1
             }
     },
     "event2":{
             "titre":"papier",
             "oui":{
-                    "pop":250,"legalite":50
+                    "pop":250,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":50,"legalite":50
+                    "pop":50,"legalite":50, "temps":1
             }
     },
     "event3":{
             "titre":"salon",
             "oui":{
-                    "pop":450,"legalite":50
+                    "pop":450,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":100,"legalite":50
+                    "pop":100,"legalite":50, "temps":1
             }
     },
     "event4":{
             "titre":"poubelle",
             "oui":{
-                    "pop":250,"legalite":50
+                    "pop":250,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":100,"legalite":50
+                    "pop":100,"legalite":50, "temps":1
             }
     },
     "event5":{
             "titre":"degradation",
             "oui":{
-                    "pop":500,"legalite":50
+                    "pop":500,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":50,"legalite":50
+                    "pop":50,"legalite":50, "temps":1
             }
     },
     "event6":{
             "titre":"argent",
             "oui":{
-                    "pop":400,"legalite":50
+                    "pop":400,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":300,"legalite":50
+                    "pop":300,"legalite":50, "temps":1
             }
     },
     "event7":{
             "titre":"election",
             "oui":{
-                    "pop":450,"legalite":50
+                    "pop":450,"legalite":50, "temps":4
             },
             "non":{
-                    "pop":50,"legalite":50
+                    "pop":50,"legalite":50, "temps":1
             }
     },
     }
@@ -81,64 +81,64 @@ class ListeEvenement:
      "event1":{
             "titre":"loi",
             "oui":{
-                    "pop":30000,"legalite":50
+                    "pop":30000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":4000,"legalite":50
+                    "pop":4000,"legalite":50, "temps":1
             }
     },
     "event2":{
             "titre":"arret",
             "oui":{
-                    "pop":30000,"legalite":50
+                    "pop":30000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":3000,"legalite":50
+                    "pop":3000,"legalite":50, "temps":1
             }
     },
     "event3":{
             "titre":"proces",
             "oui":{
-                    "pop":3000,"legalite":50
+                    "pop":3000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":600,"legalite":50
+                    "pop":600,"legalite":50, "temps":1
             }
     },
     "event4":{
             "titre":"maltraitance",
             "oui":{
-                    "pop":250000,"legalite":50
+                    "pop":250000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":500,"legalite":50
+                    "pop":500,"legalite":50, "temps":1
             }
     },
     "event5":{
             "titre":"ecole",
             "oui":{
-                    "pop":7000,"legalite":50
+                    "pop":7000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":3000,"legalite":50
+                    "pop":3000,"legalite":50, "temps":1
             }
     },
     "event6":{
             "titre":"budjet",
             "oui":{
-                    "pop":4000,"legalite":50
+                    "pop":4000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":5000,"legalite":50
+                    "pop":5000,"legalite":50, "temps":1
             }
     },
     "event7":{
             "titre":"parite",
             "oui":{
-                    "pop":170000,"legalite":50
+                    "pop":170000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":600,"legalite":50
+                    "pop":600,"legalite":50, "temps":1
             }
     },
     }
@@ -148,64 +148,64 @@ class ListeEvenement:
      "event1":{
             "titre":"confiner",
             "oui":{
-                    "pop":350500,"legalite":50
+                    "pop":350500,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":671428,"legalite":50
+                    "pop":671428,"legalite":50, "temps":1
             }
     },
     "event2":{
             "titre":"plastique",
             "oui":{
-                    "pop":350000,"legalite":50
+                    "pop":350000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":671428,"legalite":50
+                    "pop":671428,"legalite":50, "temps":1
             }
     },
     "event3":{
             "titre":"musee",
             "oui":{
-                    "pop":671428,"legalite":50
+                    "pop":671428,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":450500,"legalite":50
+                    "pop":450500,"legalite":50, "temps":1
             }
     },
     "event4":{
             "titre":"travail",
             "oui":{
-                    "pop":750000,"legalite":50
+                    "pop":750000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":671428,"legalite":50
+                    "pop":671428,"legalite":50, "temps":1
             }
     },
     "event5":{
             "titre":"argent",
             "oui":{
-                    "pop":671428,"legalite":50
+                    "pop":671428,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":950700,"legalite":50
+                    "pop":950700,"legalite":50, "temps":1
             }
     },
     "event6":{
             "titre":"animaux",
             "oui":{
-                    "pop":4700400,"legalite":50
+                    "pop":4700400,"legalite":50, "temps":4
             },
             "non":{
-                    "pop":671408,"legalite":50
+                    "pop":671408,"legalite":50, "temps":1
             }
     },
     "event7":{
             "titre":"jeune",
             "oui":{
-                    "pop":450050,"legalite":50
+                    "pop":450050,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":671428,"legalite":50
+                    "pop":671428,"legalite":50, "temps":1
             }
     },
     }
@@ -215,64 +215,64 @@ class ListeEvenement:
      "event1":{
             "titre":"egalite",
             "oui":{
-                    "pop":17654854,"legalite":50
+                    "pop":17654854,"legalite":50, "temps":4
             },
             "non":{
-                    "pop":5000000,"legalite":50
+                    "pop":5000000,"legalite":50, "temps":1
             }
     },
     "event2":{
             "titre":"vacances",
             "oui":{
-                    "pop":5000000,"legalite":50
+                    "pop":5000000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":4945945,"legalite":50
+                    "pop":4945945,"legalite":50, "temps":1
             }
     },
     "event3":{
             "titre":"villa",
             "oui":{
-                    "pop":5000000,"legalite":50
+                    "pop":5000000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":4845945,"legalite":50
+                    "pop":4845945,"legalite":50, "temps":1
             }
     },
     "event4":{
             "titre":"detenus",
             "oui":{
-                    "pop":5000000,"legalite":50
+                    "pop":5000000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":7400500,"legalite":50
+                    "pop":7400500,"legalite":50, "temps":1
             }
     },
     "event5":{
             "titre":"avortement",
             "oui":{
-                    "pop":5000000,"legalite":50
+                    "pop":5000000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":15748965,"legalite":50
+                    "pop":15748965,"legalite":50, "temps":1
             }
     },
     "event6":{
             "titre":"victimes",
             "oui":{
-                    "pop":13999000,"legalite":50
+                    "pop":13999000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":5000000,"legalite":50
+                    "pop":5000000,"legalite":50, "temps":1
             }
     },
     "event7":{
             "titre":"reforme",
             "oui":{
-                    "pop":2645713,"legalite":50
+                    "pop":2645713,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":5000000,"legalite":50
+                    "pop":5000000,"legalite":50, "temps":1
             }
     },
     }
@@ -282,64 +282,64 @@ class ListeEvenement:
      "event1":{
             "titre":"peine",
             "oui":{
-                    "pop":300000000,"legalite":50
+                    "pop":300000000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":160475284,"legalite":50
+                    "pop":160475284,"legalite":50, "temps":1
             }
     },
     "event2":{
             "titre":"attaque",
             "oui":{
-                    "pop":300000000,"legalite":50
+                    "pop":300000000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":100010200,"legalite":50
+                    "pop":100010200,"legalite":50, "temps":1
             }
     },
     "event3":{
             "titre":"routes",
             "oui":{
-                    "pop":30000000,"legalite":50
+                    "pop":30000000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":87676450,"legalite":50
+                    "pop":87676450,"legalite":50, "temps":1
             }
     },
     "event4":{
             "titre":"frontiere",
             "oui":{
-                    "pop":30000000,"legalite":50
+                    "pop":30000000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":98550050,"legalite":50
+                    "pop":98550050,"legalite":50, "temps":1
             }
     },
     "event5":{
             "titre":"vaccin",
             "oui":{
-                    "pop":158309206,"legalite":50
+                    "pop":158309206,"legalite":50, "temps":4
             },
             "non":{
-                    "pop":30000000,"legalite":50
+                    "pop":30000000,"legalite":50, "temps":1
             }
     },
     "event6":{
             "titre":"purge",
             "oui":{
-                    "pop":30000000,"legalite":50
+                    "pop":30000000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":76467889,"legalite":50
+                    "pop":76467889,"legalite":50, "temps":1
             }
     },
     "event7":{
             "titre":"retraite",
             "oui":{
-                    "pop":30000000,"legalite":50
+                    "pop":30000000,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":66467889,"legalite":50
+                    "pop":66467889,"legalite":50, "temps":1
             }
     },
     }
@@ -349,64 +349,64 @@ class ListeEvenement:
      "event1":{
             "titre":"marcial",
             "oui":{
-                    "pop":3964285714,"legalite":50
+                    "pop":3964285714,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":98555674,"legalite":50
+                    "pop":98555674,"legalite":50, "temps":1
             }
     },
     "event2":{
             "titre":"invasion",
             "oui":{
-                    "pop":2596846754,"legalite":50
+                    "pop":2596846754,"legalite":50,"temps":4
             },
             "non":{
-                    "pop":3964285714,"legalite":50
+                    "pop":3964285714,"legalite":50, "temps":1
             }
     },
     "event3":{
             "titre":"autre",
             "oui":{
-                    "pop":3964285714,"legalite":50
+                    "pop":3964285714,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":44846754,"legalite":50
+                    "pop":44846754,"legalite":50, "temps":1
             }
     },
     "event4":{
             "titre":"virus",
             "oui":{
-                    "pop":3964285714,"legalite":50
+                    "pop":3964285714,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":61942304,"legalite":50
+                    "pop":61942304,"legalite":50, "temps":1
             }
     },
     "event5":{
             "titre":"survivant",
             "oui":{
-                    "pop":3964285714,"legalite":50
+                    "pop":3964285714,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":44850250,"legalite":50
+                    "pop":44850250,"legalite":50,"temps":1
             }
     },
     "event6":{
             "titre":"espace",
             "oui":{
-                    "pop":13467864,"legalite":50
+                    "pop":13467864,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":3964285714,"legalite":50
+                    "pop":3964285714,"legalite":50,"temps":1
             }
     },
     "event7":{
             "titre":"bombe",
             "oui":{
-                    "pop":3964285714,"legalite":50
+                    "pop":3964285714,"legalite":50, "temps":1
             },
             "non":{
-                    "pop":3457864,"legalite":50
+                    "pop":3457864,"legalite":50, "temps":1
             }
     },
     }
