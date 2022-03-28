a481258a2b00ae06d401b64535ff2e4e7c9514ff
QuentinBubu <QuentinBubu.dev@gmail.com>
2022-03-28 21:53:16 +0200

    Ajout de constante

--- src/utils/constante.py
+++ src/utils/constante.py
@@ -1,14 +1,10 @@
+CITOYEN = "citoyen"
 MAIRE = "maire"
 DEPUTE = "député"
 DEPUTE_REGIONAL = "député régional"
 MINISTRE = "ministre"
 PRESIDENT = "président"
 PRESIDENT_DES_NATIONS = "président des nations"
-
-"""
-class Population(Jauge):
-
-    def __init__ (self, popularite):
-        super().__init__()
-
-"""
+FIN_GAGNE = 0
+FIN_TEMPS = 1
+FIN_PRISON = 2
