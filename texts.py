108857ce46d3a9d56b8560c0fc30ddba49be6d11
QuentinBubu <QuentinBubu.dev@gmail.com>
2022-03-28 22:23:55 +0200

    Re localisation de la classe temps

--- texts.py
+++
@@ -1,6 +0,0 @@
-PROLOGUE = """
-Il était une fois, un simple citoyen
-"""
-T_FIN_GAGNE  = "Vous avez gagné!"
-T_FIN_TEMPS = "Malheuresement vous êtes décédés"
-T_FIN_PRISON = "Dommage, la justice vous a rattrapée, vous allez au séchoir"
