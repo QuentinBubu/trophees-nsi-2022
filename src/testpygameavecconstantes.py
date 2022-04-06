33d402d7e623ab524d5039f519d647d7685d18cb
Léopold <100198179+LDEM26@users.noreply.github.com>
2022-04-06 18:49:03 +0200

    ajout dossier pour afficher les jauges et le fond

---
+++ src/testpygameavecconstantes.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  1 11:35:08 2022
+
+@author: leopold.demarco
+"""
+
+import pygame 
+
+pygame.init()
+
+screen = pygame.display.set_mode((1300 ,900), pygame.RESIZABLE)
+
+ouvert = True
+
+n = 1
+
+while ouvert: 
+    fond = pygame.image.load('image/imagepygame.jpg').convert_alpha()
+    screen.blit(fond, (0,0))
+    pygame.draw.rect(screen , (236, 16, 16), (560 ,30 ,n ,30), 0) #A la place de 2*30 -> Legalite.get_leg() 
+    pygame.draw.rect(screen , (16, 119, 236), (560 ,100 ,n ,30), 0) #A la place de 270 -> Justice.get_jus()    
+    pygame.draw.rect(screen , (247, 232, 18), (560 ,170 , 7000/(20000/100) ,30), 0) #A la place de 5000/(20000/100) -> Population.get_pop()
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP :
+            ouvert = False
+        if event.type == pygame.KEYUP :
+            n += 1
+pygame.quit()
