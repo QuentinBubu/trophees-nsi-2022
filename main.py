e3cd7843e1c4b4588bea6f254cd1eb4127fdf1ac
QuentinBubu <QuentinBubu.dev@gmail.com>
2022-05-01 20:17:44 +0200

    Re placement

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ while ouvert:               #Boucle qui garde la fenêtre ouverte
                             bouton.set_clicked(False)
 
     if ecran == screen.PROLOGUE:                        #Ecran de prologue
-        afficher_text(T_PROLOGUE, screen, screen.font, screen.PROLOGUE, (0.5, 0.5), True, NOIR)
+        afficher_text(T_PROLOGUE, screen, screen.font, screen.PROLOGUE, (0.2, 0.3), True, NOIR)
 
     elif ecran == screen.MAIN: # en attente de réponse
 
